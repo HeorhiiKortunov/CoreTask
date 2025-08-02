cab78f1050d64f0b67fe90cbc08f1ca835800cba
Heorhii Kortunov <heorhiikortunov@gmail.com>
2025-08-02 23:41:04 +0200

    Initial project

---
+++ src/main/java/com/example/maven/CoreTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreTaskApplication.class, args);
+	}
+
+}
