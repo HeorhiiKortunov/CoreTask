c51f29005493679b30dfd932f103d5cb2693f9d2
Heorhii Kortunov <heorhiikortunov@gmail.com>
2025-08-03 22:27:46 +0200

    Added CustomUserDetailsService

---
+++ src/main/java/com/example/maven/security/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.example.maven.security;
+
+import com.example.maven.persistence.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+	private final UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		var user = userRepository.findByUsername(username)
+				.orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+
+		return UserPrincipal.builder()
+				.id(user.getId())
+				.username(user.getUsername())
+				.roles(user.getRoles())
+				.password(user.getPassword())
+				.build();
+	}
+}
