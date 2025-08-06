0c46d2b1aa0e1636cf65e87f0bd36d19ce145aa4
Heorhii Kortunov <heorhiikortunov@gmail.com>
2025-08-06 21:03:38 +0200

    Added ProjectController

---
+++ src/main/java/com/example/maven/api/controller/ProjectController.java
@@ -0,0 +1,45 @@
+package com.example.maven.api.controller;
+
+import com.example.maven.api.dto.request.project.ProjectCreateDto;
+import com.example.maven.api.dto.request.project.ProjectUpdateDto;
+import com.example.maven.api.dto.response.ProjectResponseDto;
+import com.example.maven.service.ProjectService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/projects")
+public class ProjectController {
+	private final ProjectService projectService;
+
+	@PostMapping
+	public ResponseEntity<ProjectResponseDto> createProject(@RequestBody ProjectCreateDto dto){
+		return ResponseEntity.ok(projectService.createProject(dto));
+	}
+
+	@GetMapping
+	public ResponseEntity<List<ProjectResponseDto>> getAllProjects(){
+		return ResponseEntity.ok(projectService.findAllProjects());
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<ProjectResponseDto> getProject(@PathVariable long id){
+		return ResponseEntity.ok(projectService.findById(id));
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<ProjectResponseDto> updateProject(@PathVariable long id, @RequestBody ProjectUpdateDto dto){
+		return ResponseEntity.ok(projectService.updateProject(id, dto));
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteProject(@PathVariable long id){
+		projectService.deleteProject(id);
+		return ResponseEntity.noContent().build();
+	}
+
+}
