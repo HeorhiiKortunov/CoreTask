462e60a7d82d552c931ccc4450e7ca5eeb923bbb
Heorhii Kortunov <heorhiikortunov@gmail.com>
2025-08-08 18:56:28 +0200

    Updated GlobalExceptionHandler to return a json instead of a string

--- src/main/java/com/example/maven/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/maven/exception/GlobalExceptionHandler.java
@@ -5,11 +5,34 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.nio.file.AccessDeniedException;
+import java.time.LocalDateTime;
+import java.util.Map;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
 	@ExceptionHandler(ResourceNotFoundException.class)
-	public ResponseEntity<String> handleResourceNotFound(ResourceNotFoundException ex) {
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+	public ResponseEntity<Map<String, Object>> handleResourceNotFound(ResourceNotFoundException ex) {
+		return ResponseEntity
+				.status(HttpStatus.FORBIDDEN)
+				.body(Map.of(
+						"timestamp", LocalDateTime.now().toString(),
+						"status", HttpStatus.NOT_FOUND.value(),
+						"error", HttpStatus.NOT_FOUND.getReasonPhrase(),
+						"message", ex.getMessage()
+				));
+	}
+
+	@ExceptionHandler(AccessDeniedException.class)
+	public ResponseEntity<Map<String, Object>> handleAccessDenied(AccessDeniedException ex) {
+		return ResponseEntity
+				.status(HttpStatus.FORBIDDEN)
+				.body(Map.of(
+						"timestamp", LocalDateTime.now().toString(),
+						"status", HttpStatus.FORBIDDEN.value(),
+						"error", HttpStatus.FORBIDDEN.getReasonPhrase(),
+						"message", ex.getMessage()
+				));
 	}
 }
