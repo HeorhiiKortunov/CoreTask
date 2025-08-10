07131c5d375dc8a09ece4ae4033ed10ad8419b33
Heorhii Kortunov <heorhiikortunov@gmail.com>
2025-08-11 00:06:17 +0200

    Added docker support

--- src/test/java/com/example/maven/CoreTaskApplicationTests.java
+++ src/test/java/com/example/maven/CoreTaskApplicationTests.java
@@ -1,9 +1,8 @@
 package com.example.maven;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class CoreTaskApplicationTests {
 
 	@Test
